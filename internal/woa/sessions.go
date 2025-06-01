399b68f89bc9bd4dfdd66d06cd6ef5717859d1ea
CircuitCamel <husainadwan0@gmail.com>
2025-06-01 04:56:21 +0300

    WORKING SESSIONS

--- internal/woa/sessions.go
+++ internal/woa/sessions.go
@@ -1,7 +1,70 @@
 package woa
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"text/template"
+	"warofages/internal/util"
+)
 
-func Sessions(w http.ResponseWriter, r *http.Request) {
+func SessionHandler(w http.ResponseWriter, r *http.Request) {
+	sessionDetailHandler(w, r)
+}
 
+func sessions(w http.ResponseWriter, r *http.Request) {
+	sessions, err := getSessions()
+	if err != nil {
+		return
+	}
+	tmpl, err := template.ParseFiles("website/sessions/index.html")
+	if err != nil {
+		return
+	}
+	tmpl.Execute(w, sessions)
+}
+
+func sessionDetailHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		// No ID provided — serve sessions list
+		sessions(w, r)
+		return
+	}
+
+	sessionID, _ := strconv.Atoi(id)
+
+	tmpl, err := template.ParseFiles("./website/sessions/session.html")
+	if err != nil {
+		http.Error(w, "Template error", http.StatusInternalServerError)
+		return
+	}
+
+	sessions, _ := getSessions()
+
+	var selected Session
+	for _, a := range sessions {
+		if a.ID == sessionID {
+			selected = a
+		}
+	}
+
+	databytes, _ := os.ReadFile(selected.Path)
+
+	selected.Data = util.MdToHTML(databytes)
+
+	tmpl.Execute(w, selected)
+}
+
+func getSessions() ([]Session, error) {
+	files, err := filepath.Glob("./md/sessions/*.md")
+	if err != nil {
+		return nil, err
+	}
+	result := make([]Session, len(files))
+	for i, v := range files {
+		result[i] = Session{ID: i, Path: v}
+	}
+	return result, nil
 }
---
+++ internal/woa/struct.go
@@ -0,0 +1,19 @@
+package woa
+
+type Session struct {
+	ID   int
+	Path string
+	Data string
+}
+
+type Rule struct {
+	ID   int
+	Path string
+	Data string
+}
+
+type Player struct {
+	ID   int
+	Path string
+	Data string
+}
