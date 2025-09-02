4e4b840a73020f1371deaae72b773013160a91d1
CircuitCamel <cc@caml.cc>
2025-09-02 10:14:22 +0300

    formatting

--- static/scripts/breadcrumb.js
+++ static/scripts/breadcrumb.js
@@ -7,10 +7,10 @@ let url = '';
 
 for (let i = 0; i < path.length; i++) {
     url += '/' + path[i];
-    let text = decodeURIComponent(path[i]).replace(/-/g, ' ').split(' ').map(function(word) {
+    let text = decodeURIComponent(path[i]).replace(/-/g, ' ').split(' ').map(function (word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }).join(' ');
-    
+
     if (i === path.length - 1) {
         breadcrumbItems.push(`<span aria-current="page">${text}</span>`);
     } else {
